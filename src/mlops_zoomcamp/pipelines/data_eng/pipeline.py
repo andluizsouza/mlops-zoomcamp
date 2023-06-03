d3285583de76f47bdb10a119c12f53b55e224311
Anderson Souza <33399123+andluizsouza@users.noreply.github.com>
2023-06-03 21:01:10 +0000

    Connecting nodes

--- src/mlops_zoomcamp/pipelines/data_eng/pipeline.py
+++ src/mlops_zoomcamp/pipelines/data_eng/pipeline.py
@@ -23,11 +23,60 @@ def create_pipeline() -> Pipeline:
 
 
 def create_pipeline(**kwargs) -> Pipeline:
+
     return pipeline([
         node(
             func=calc_duration,
-            inputs=["train_raw", "params:data_preprocess"],
-            outputs="train_inter",
+            inputs=["train_dataset_raw", "params:data_preprocess"],
+            outputs="train_dataset_inter",
             name="calc_duration_train",
-        )
+        ),
+        node(
+            func=calc_duration,
+            inputs=["val_dataset_raw", "params:data_preprocess"],
+            outputs="val_dataset_inter",
+            name="calc_duration_val",
+        ),
+        node(
+            func=calc_duration,
+            inputs=["test_dataset_raw", "params:data_preprocess"],
+            outputs="test_dataset_inter",
+            name="test_duration_test",
+        ),
+        node(
+            func=set_categorical_features,
+            inputs=["train_dataset_inter", "params:data_preprocess"],
+            outputs="train_dataset_primary",
+            name="set_categorical_train",
+        ),
+        node(
+            func=set_categorical_features,
+            inputs=["val_dataset_inter", "params:data_preprocess"],
+            outputs="val_dataset_primary",
+            name="set_categorical_val",
+        ),
+        node(
+            func=set_categorical_features,
+            inputs=["test_dataset_inter", "params:data_preprocess"],
+            outputs="test_dataset_primary",
+            name="set_categorical_test",
+        ),
+        node(
+            func=create_train_set,
+            inputs=["train_dataset_primary", "params:data_preprocess"],
+            outputs=["x_train", "y_train", "dv"],
+            name="create_train_set",
+        ),
+        node(
+            func=create_val_test_sets,
+            inputs=["val_dataset_primary", "params:data_preprocess"],
+            outputs=["x_val", "y_val"],
+            name="create_val_set",
+        ),
+        node(
+            func=create_val_test_sets,
+            inputs=["test_dataset_primary", "params:data_preprocess"],
+            outputs=["x_test", "y_test"],
+            name="create_test_set",
+        ),
     ])
